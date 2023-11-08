f20120f706249965156e62ba93721dc364c13f5e
NdaaTzyyy <96452047+NdaaTzyyy@users.noreply.github.com>
2023-11-08 12:07:11 +0000

    y

--- all/settings.js
+++ all/settings.js
@@ -1,18 +1,18 @@
 require("./module")
 
-global.owner = "6289508082845"
-global.creAtor = "6289508082845@s.whatsapp.net"
-global.namaowner = "KOI"
-global.namabot = "Lumine-MD"
+global.owner = "6285640575421"
+global.creAtor = "6285640575421@s.whatsapp.net"
+global.namaowner = "Ndaa"
+global.namabot = "NdaaBotz-Md"
 global.thumb = fs.readFileSync("./thumb.png")
 global.thumbqris = fs.readFileSync("./qris.jpg")
-global.packname = "Lumine-MD"
+global.packname = "NdaaBotz-Md"
 global.author = `Date: ${moment.tz('Asia/Jakarta').format('DD/MM/YY')}\nYouTube: QyuuNee\nBot: 0895-3253-44177`
-global.nameGEDE = "KOI"
-global.ovo = "0895-0808-2845" 
-global.gopay = "0895-0808-2845"
-global.shopeepay = "0895-0808-2845"
-global.pulsatri = "0895-0808-2845"
+global.nameGEDE = "NDAA"
+global.ovo = "-" 
+global.gopay = "-"
+global.shopeepay = "-"
+global.pulsatri = "-"
 global.phs = "•"
 global.prefa = ['', '.']
 global.antilink = false
